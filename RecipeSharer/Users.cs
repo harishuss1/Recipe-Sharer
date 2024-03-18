c057cdab03b792be0898cda19c870ff717e5c40f
unknown <manil.hmd@gmail.com>
2024-03-17 20:30:26 -0400

    added method headers

--- RecipeSharer/Users.cs
+++ RecipeSharer/Users.cs
@@ -1,20 +1,35 @@
-public class Users
+public class User
 {
 // admin user? 
 // maybe another class for usersOperation if we think there are too many methods in this class
 // public class Users
 
-// username property ? or name property
+    // username property ? or name property
+    public string Username;
 
-// addUser method
+    private string Password;
 
-// removeUser method
+    private string? ProfilePicturePath;
 
-// removeProfilePicture method
+    private string? Description;
 
-// removePersonalDescription method
+    // Constructor
+    public User(string name, string password);
 
-// updatePassword method
+    // updatePassword method
+    public void updatePassword(string newPassword);
+
+    //// These all will probably just end up being setters for the properties
+    // Add Profile picture
+    public void addProfilePicture(string path);
+    // removeProfilePicture method
+    public void removeProfilePicture();
+    // update pfp
+    public void changeProfilePicture(string path);
+    // add description
+    public void addDescription(string description);
+    // update description
+    public void updateDescription(string description);
 
 // Some methods might be moved to UserOperations not 100% sure.
 }
