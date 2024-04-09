45aa7e38ca1a063f1f7d43f81c16b80bacb344c1
Amy Tan <amytan211@gmail.com>
2024-04-09 16:54:09 -0400

    wrote some properties

--- RecipeSharer/Recipes.cs
+++ RecipeSharer/Recipes.cs
@@ -1,11 +1,25 @@
 public class Recipes
 {
 // recipe name property 
+    public string RecipeName { get; set; }
 // description property
+    public string RecipeDesc { get; set; }
 // ingredients property
+    public List<string> RecipeIngredients { get; }
 // owner property
-// instructions property 
+    public string RecipeOwner { get; set; }
+// instructions property
+    public string RecipeInstructions { get; set; }
 // rating property
+    public int RecipeRating { get; set; }
+// tags
+    public List<string> RecipeTag { get; set;}
+// prep time
+    public int PrepTime { get; set; }
+// cooking time
+    public int CookingTime { get; set; }
+// servings
+    public int NumOfServings {get;}
 
 // addRecipe
 
