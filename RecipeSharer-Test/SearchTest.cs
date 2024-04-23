a0fecab01b90199fc34b5ed1dcb5e7fd8c608336
Haris Hussain <hussainharis170@gmail.com>
2024-04-22 20:54:12 -0400

    fixed all errors

--- RecipeSharer-Test/SearchTest.cs
+++ RecipeSharer-Test/SearchTest.cs
@@ -2,96 +2,96 @@ using Recipes;
 using RecipeSearch;
 using Users;
 namespace RecipeSharerTests;
+
 [TestClass]
-public class SearchTests
-{
-    private Search _search;
-    private List<Recipe> _recipes;
-    private User _user;
-
-    [TestInitialize]
-    public void TestInitialize()
+    public class SearchTests
     {
-        _search = new Search();
-        _user = new User("ChefGourmet", "password123");
+        private Search _search;
+        private List<Recipe> _recipes;
+        private User _testUser;
 
-        _recipes = new List<Recipe>
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _search = new Search();
+            _testUser = new User("ChefGourmet", "password123");  
+
+            _recipes = new List<Recipe>
             {
-                new Recipe(_user, "Apple Pie")
+                new Recipe(_testUser, "Chocolate Cake", "Delicious dark chocolate cake", TimeSpan.FromMinutes(20), TimeSpan.FromHours(1), 4) 
                 {
-                    Ingredients = new List<Ingredient> { new Ingredient("Apple", "2 pieces", "Fruit"), new Ingredient("Sugar", "100g", "Condiment") },
-                    Tags = new List<string> { "Dessert", "Baking" },
-                    PreparationTime = TimeSpan.FromMinutes(30),
-                    CookingTime = TimeSpan.FromMinutes(60),
-                    Servings = 4
+                    Ingredients = new List<Ingredient> { new Ingredient("Chocolate", 200, "g", "Solid") },
+                    Tags = new List<string> { "Dessert" },
+                    Ratings = new List<Rating> { new Rating(_testUser, 5) }  
                 },
-                new Recipe(_user, "Tomato Soup")
+                new Recipe(_testUser, "Tomato Soup", "Fresh and creamy tomato soup", TimeSpan.FromMinutes(10), TimeSpan.FromMinutes(30), 2) 
                 {
-                    Ingredients = new List<Ingredient> { new Ingredient("Tomato", "5 pieces", "Vegetable"), new Ingredient("Salt", "5g", "Condiment") },
+                    Ingredients = new List<Ingredient> { new Ingredient("Tomato", 5, "pcs", "Vegetable") },
                     Tags = new List<string> { "Soup", "Vegan" },
-                    PreparationTime = TimeSpan.FromMinutes(10),
-                    CookingTime = TimeSpan.FromMinutes(20),
-                    Servings = 2
+                    Ratings = new List<Rating> { new Rating(_testUser, 4) }  
                 }
             };
+        }
+
+        [TestMethod]
+        public void AddTag_ValidTag_ShouldAddTag()
+        {
+            _search.AddTag("Vegan");
+            Assert.IsTrue(_search.Tags.Contains("Vegan"));
+        }
+
+        [TestMethod]
+        public void SetIngredients_ValidIngredients_ShouldSetIngredients()
+        {
+            var ingredients = new List<string> { "Chocolate", "Tomato" };
+            _search.SetIngredients(ingredients);
+            Assert.IsTrue(_search.Ingredients.SequenceEqual(ingredients));
+        }
+
+        [TestMethod]
+        public void SetKeyword_ValidKeyword_ShouldSetKeyword()
+        {
+            var keyword = "Cake";
+            _search.SetKeyword(keyword);
+            Assert.AreEqual(keyword, _search.Keyword);
+        }
+
+        [TestMethod]
+        public void SetTimeConstraints_ValidTimes_ShouldSetTimeConstraints()
+        {
+            var minDuration = TimeSpan.FromMinutes(20);
+            var maxDuration = TimeSpan.FromHours(2);
+            _search.SetTimeConstraints(minDuration, maxDuration);
+            Assert.AreEqual(minDuration, _search.MinDuration);
+            Assert.AreEqual(maxDuration, _search.MaxDuration);
+        }
+
+        [TestMethod]
+        public void SetServingsConstraints_ValidServings_ShouldSetServingsConstraints()
+        {
+            _search.SetServingsConstraints(1, 10);
+            Assert.AreEqual(1, _search.MinServings);
+            Assert.AreEqual(10, _search.MaxServings);
+        }
+
+        [TestMethod]
+        public void SetMinimumRating_ValidRating_ShouldSetMinimumRating()
+        {
+            _search.SetMinimumRating(3.5);
+            Assert.AreEqual(3.5, _search.MinimumRating);
+        }
+
+        [TestMethod]
+        public void PerformSearch_WithCriteria_ShouldReturnFilteredRecipes()
+        {
+            _search.SetIngredients(new List<string> { "Chocolate" });
+            _search.AddTag("Dessert");
+            _search.SetKeyword("Cake");
+
+            var result = _search.PerformSearch(_recipes);
+
+            Assert.IsTrue(result.Any(), "Should return at least one recipe.");
+            Assert.IsTrue(result.All(r => r.Name.Contains("Chocolate Cake")), "Should return recipes containing 'Chocolate Cake'.");
+        }
     }
-
-    [TestMethod]
-    public void AddTag_ValidTag_ShouldSucceed()
-    {
-        _search.AddTag("Dessert");
-        Assert.IsTrue(_search.Tags.Contains("Dessert"));
-    }
-
-    [TestMethod]
-    public void SetIngredients_ValidIngredients_ShouldSucceed()
-    {
-        List<string> ingredients = new List<string> { "Apple", "Tomato" };
-        _search.SetIngredients(ingredients);
-        Assert.IsTrue(_search.Ingredients.SequenceEqual(ingredients));
-    }
-
-    [TestMethod]
-    public void SetKeyword_ValidKeyword_ShouldSucceed()
-    {
-        string keyword = "Pie";
-        _search.SetKeyword(keyword);
-        Assert.AreEqual(keyword, _search.Keyword);
-    }
-
-    [TestMethod]
-    public void PerformSearch_WithCriteria_ShouldReturnExpectedResults()
-    {
-        _search.SetIngredients(new List<string> { "Apple" });
-        _search.AddTag("Dessert");
-        _search.SetKeyword("Pie");
-
-        var results = _search.PerformSearch(_recipes);
-
-        Assert.AreEqual(1, results.Count);
-        Assert.AreEqual("Apple Pie", results[0].Name);
-    }
-
-    [TestMethod]
-    public void SetTimeConstraints_ValidTimes_ShouldSucceed()
-    {
-        _search.SetTimeConstraints(TimeSpan.FromMinutes(50), TimeSpan.FromMinutes(120));
-        Assert.AreEqual(TimeSpan.FromMinutes(50), _search.MinDuration);
-        Assert.AreEqual(TimeSpan.FromMinutes(120), _search.MaxDuration);
-    }
-
-    [TestMethod]
-    public void SetServingsConstraints_ValidRange_ShouldSucceed()
-    {
-        _search.SetServingsConstraints(1, 5);
-        Assert.AreEqual(1, _search.MinServings);
-        Assert.AreEqual(5, _search.MaxServings);
-    }
-
-    [TestMethod]
-    public void SetMinimumRating_ValidRating_ShouldSucceed()
-    {
-        _search.SetMinimumRating(3.5);
-        Assert.AreEqual(3.5, _search.MinimumRating);
-    }
-}
