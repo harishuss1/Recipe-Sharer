85bdc39f88d2b284dfa983973abfa6b805a8d080
Haris Hussain <hussainharis170@gmail.com>
2024-05-02 23:23:31 -0400

    unit testing for RatingOperations should now be complete

--- RecipeSharer-Test/RatingOperationsTest.cs
+++ RecipeSharer-Test/RatingOperationsTest.cs
@@ -1,100 +1,157 @@
+using Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Recipes;
 using Users;
-
+using Moq;
+using System.Linq.Expressions;
 [TestClass]
 public class RatingOperationsTests
 {
-    private RatingOperations _ratingOperations;
     private User _user;
     private Recipe _recipe;
 
-    [TestInitialize]
 
-    public void TestInitialize()
+    private static (Mock<RecipeSharerContext>, Mock<DbSet<Rating>>) GetMocks()
     {
-        _ratingOperations = new RatingOperations();
-        _user = new User("chefJohn", "securepassword123");  // Assuming User and Recipe constructors
-        _recipe = new Recipe(
-            _user,
-            "Chocolate Cake",
-            "Delicious and rich chocolate cake.",
-            new List<Ingredient>(),
-            TimeSpan.FromMinutes(30),
-            TimeSpan.FromMinutes(60),
-            4
-        );
+        var mockContext = new Mock<RecipeSharerContext>();
+        var mockRatings = new Mock<DbSet<Rating>>();
+        mockContext.Setup(mock => mock.Ratings).Returns(mockRatings.Object);
+
+        return (mockContext, mockRatings);
     }
+
+    private static void ConfigureDbSetMock<T>(IQueryable<T> data, Mock<DbSet<T>> mockDbSet) where T : class
+    {
+        mockDbSet.As<IQueryable<T>>().Setup(mock => mock.Provider).Returns(data.Provider);
+        mockDbSet.As<IQueryable<T>>().Setup(mock => mock.Expression).Returns(data.Expression);
+        mockDbSet.As<IQueryable<T>>().Setup(mock => mock.ElementType).Returns(data.ElementType);
+        mockDbSet.As<IQueryable<T>>().Setup(mock => mock.GetEnumerator()).Returns(data.GetEnumerator());
+    }
+
+    [TestInitialize]
+public void TestInitialize()
+{
+    _user = new User
+    {
+        UserId = 1,
+        Username = "Test User"
+    };
+
+    _recipe = new Recipe
+    {
+        RecipeId = 1,
+        Name = "Test Recipe"
+    };
+
+}
 
     [TestMethod]
     public void AddRating_ValidInputs_ShouldAddRating()
     {
         // Arrange
-        int initialCount = _recipe.Ratings.Count;
+        var ratings = new List<Rating>().AsQueryable();
+        var (mockContext, mockRatings) = GetMocks();
+        ConfigureDbSetMock(ratings, mockRatings);
+        var ratingOperations = new RatingOperations(mockContext.Object);
 
         // Act
-        _ratingOperations.AddRating(_user, _recipe, 5);
+        ratingOperations.AddRating(_user, _recipe, 5);
 
         // Assert
-        Assert.AreEqual(initialCount + 1, _recipe.Ratings.Count);
-        Assert.AreEqual(5, _recipe.Ratings.Last().Score);
+        mockRatings.Verify(m => m.Add(It.IsAny<Rating>()), Times.Once);
+        mockContext.Verify(m => m.SaveChanges(), Times.Once);
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void AddRating_InvalidRating_ShouldThrowArgumentException()
     {
+        // Arrange
+        var ratings = new List<Rating>().AsQueryable();
+        var (mockContext, mockRatings) = GetMocks();
+        ConfigureDbSetMock(ratings, mockRatings);
+        var ratingOperations = new RatingOperations(mockContext.Object);
+
         // Act
-        _ratingOperations.AddRating(_user, _recipe, 11); 
+        ratingOperations.AddRating(_user, _recipe, 11);
     }
 
     [TestMethod]
-    public void RemoveRating_ExistingRating_ShouldRemoveRating()
+    public void RemoveRating_RatingExists_ShouldRemoveRating()
     {
         // Arrange
-        _ratingOperations.AddRating(_user, _recipe, 8);
+        var ratings = new List<Rating>
+    {
+        new Rating { User = _user, Recipe = _recipe, Score = 5 }
+    }.AsQueryable();
+        var (mockContext, mockRatings) = GetMocks();
+        ConfigureDbSetMock(ratings, mockRatings);
+        var ratingOperations = new RatingOperations(mockContext.Object);
 
         // Act
-        _ratingOperations.RemoveRating(_user, _recipe);
+        ratingOperations.RemoveRating(_user, _recipe);
 
         // Assert
-        Assert.IsFalse(_recipe.Ratings.Any());
+        mockRatings.Verify(m => m.Remove(It.IsAny<Rating>()), Times.Once);
+        mockContext.Verify(m => m.SaveChanges(), Times.Once);
     }
 
     [TestMethod]
-    public void UpdateRating_ExistingRating_ShouldUpdateRating()
+    public void UpdateRating_RatingExists_ShouldUpdateRating()
     {
         // Arrange
-        _ratingOperations.AddRating(_user, _recipe, 6);
+        var ratings = new List<Rating>
+    {
+        new Rating { User = _user, Recipe = _recipe, Score = 5 }
+    }.AsQueryable();
+        var (mockContext, mockRatings) = GetMocks();
+        ConfigureDbSetMock(ratings, mockRatings);
+        var ratingOperations = new RatingOperations(mockContext.Object);
 
         // Act
-        _ratingOperations.UpdateRating(_user, _recipe, 9);
+        ratingOperations.UpdateRating(_user, _recipe, 8);
 
         // Assert
-        Assert.AreEqual(9, _recipe.Ratings.First().Score);
+        var updatedRating = ratings.First();
+        Assert.AreEqual(8, updatedRating.Score);
+        mockContext.Verify(m => m.SaveChanges(), Times.Once);
     }
 
     [TestMethod]
     public void ViewRating_NoRatingAvailable_ShouldReturnZero()
     {
+        // Arrange
+        var ratings = new List<Rating>().AsQueryable();
+        var (mockContext, mockRatings) = GetMocks();
+        ConfigureDbSetMock(ratings, mockRatings);
+        var ratingOperations = new RatingOperations(mockContext.Object);
+
         // Act
-        double rating = _ratingOperations.ViewRating(_recipe);
+        double rating = ratingOperations.ViewRating(_recipe);
 
         // Assert
         Assert.AreEqual(0, rating);
     }
 
     [TestMethod]
-    public void ViewRating_RatingsAvailable_ShouldReturnAvgRating()
+    public void ViewRating_RatingsAvailable_ShouldReturnAverageScore()
     {
         // Arrange
-        _ratingOperations.AddRating(_user, _recipe, 8);
-        _ratingOperations.AddRating(_user, _recipe, 6);
+        var ratings = new List<Rating>
+    {
+        new Rating { User = _user, Recipe = _recipe, Score = 5 },
+        new Rating { User = _user, Recipe = _recipe, Score = 7 },
+        new Rating { User = _user, Recipe = _recipe, Score = 9 }
+    }.AsQueryable();
+        var (mockContext, mockRatings) = GetMocks();
+        ConfigureDbSetMock(ratings, mockRatings);
+        var ratingOperations = new RatingOperations(mockContext.Object);
 
-        // Act 
-        double rating = _ratingOperations.ViewRating(_recipe);
+        // Act
+        double averageScore = ratingOperations.ViewRating(_recipe);
 
         // Assert
-        Assert.AreEqual(7, rating);
+        Assert.AreEqual(7, averageScore);
     }
 }
