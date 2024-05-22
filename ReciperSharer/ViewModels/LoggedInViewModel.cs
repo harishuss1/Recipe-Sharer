b13cbe2d581f6bbe16ee943567d81d1bc4e0928e
Haris Hussain <hussainharis170@gmail.com>
2024-05-21 23:45:22 -0400

    Homepage Fully functional

--- ReciperSharer/ViewModels/LoggedInViewModel.cs
+++ ReciperSharer/ViewModels/LoggedInViewModel.cs
@@ -11,6 +11,12 @@ public class LoggedInViewModel : ViewModelBase
   public string Greeting { get; }
 
   public ReactiveCommand<Unit, Unit> Logout { get; }
+  public ReactiveCommand<Unit, Unit> ShowRecipeCommand { get; }
+    public ReactiveCommand<Unit, Unit> ShowRatingCommand { get; }
+    public ReactiveCommand<Unit, Unit> ShowSearchCommand { get; }
+    public ReactiveCommand<Unit, Unit> ShowMakeRecipeCommand { get; }
+    public ReactiveCommand<Unit, Unit> ShowProfileCommand { get; }
+
 
   public LoggedInViewModel()
   {
